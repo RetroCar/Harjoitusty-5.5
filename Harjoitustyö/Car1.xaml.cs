8d873d1c126d638247f1ca438208193c06fe4672
Saamari <sampsa_kautto@hotmail.com>
2016-04-06 14:25:10 +0300

    UUSI JA TOIMIVA

---
+++ Harjoitustyö/Car1.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Harjoitustyö
+{
+    public sealed partial class Car1 : UserControl
+    {
+        private DispatcherTimer timer;
+        private int currentFrame = 0;
+        private int direction = 1;
+
+        //location
+        public double LocationX { get; set; }
+        public double LocationY { get; set; }
+        //Speed values
+        = 15.0;
+        private readonly double MaxSpeed = 15.0;
+        private readonly double MaxSpeed1 = 7.5;
+        private readonly double Accelarate = 3.0;
+        private readonly double Break = 1.5;
+
+        private double speed;
+        private double speed1;
+
+        // Cars angle
+        private double Angle = 0;
+        private readonly double AngleTier = 5;
+        public Car1()
+        {
+            this.InitializeComponent();
+
+            //starts the animation ?
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Car;
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 150);
+            timer.Start();
+        }
+
+        private void Timer_Car(object sender, object e)
+        {
+            //frame: 0,1,2,3,4
+            if (direction == 1) currentFrame++;
+            else currentFrame--;
+            // direction
+            if (currentFrame == 0 || currentFrame == 4) direction *= -1;
+        }
+
+        // Update position
+        public void Updateposition()
+        {
+            SetValue(Canvas.LeftProperty, LocationX);
+            SetValue(Canvas.TopProperty, LocationY);
+        }
+
+        public void Rotate(int direction)
+
+        {
+            if (speed > 1)
+            {
+                Angle += direction * AngleTier;
+                Car_1_Angle.Angle = Angle;
+            }
+
+        }
+
+        //forward movement
+        public void Drive()
+        {
+            speed += Accelarate;
+            if (speed > MaxSpeed) speed = MaxSpeed;
+            LocationX -= (Math.Cos(Math.PI / 180 * (Angle + 90))) * speed;
+            LocationY -= (Math.Sin(Math.PI / 180 * (Angle + 90))) * speed;
+
+        }
+
+
+
+        // backwards
+        public void Drive1()
+        {
+            speed1 += Break;
+            if (speed1 > MaxSpeed1) speed1 = MaxSpeed1;
+            LocationX -= (Math.Cos(Math.PI / 180 * (Angle - 90))) * speed1;
+            LocationY -= (Math.Sin(Math.PI / 180 * (Angle - 90))) * speed1;
+
+        }
+    }
+}
---
+++ Harjoitustyö/Game.xaml.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Harjoitustyö
+{
+    public sealed partial class Game : Page
+    {
+
+        // First car
+        private Car1 car1;
+
+        //enviroment
+        private Asfalt asfalt;
+        private Sand sand;
+       
+
+        private double CanvasWidth;
+        private double CanvasHeight;
+        private double CanvasWidthMap;
+        private double CanvasHeightMap;
+
+
+        // Controls
+        private bool Up;
+        private bool Left;
+        private bool Right;
+        private bool Down;
+
+        // game timer
+        private DispatcherTimer game;
+        public Game()
+        {
+            this.InitializeComponent();
+
+            // window size
+            ApplicationView.PreferredLaunchWindowingMode
+               = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            ApplicationView.PreferredLaunchViewSize = new Size(1280, 720);
+
+            //get my own canvas size
+
+            CanvasWidth = Track.Width;
+            CanvasHeight = Track.Height;
+            CanvasWidthMap = Track.Width;
+            CanvasHeightMap = Track.Height;
+
+            //add map
+            asfalt = new Asfalt { LocationX = CanvasHeightMap, LocationY = CanvasHeightMap };
+            Track.Children.Add(asfalt);
+
+            sand = new Sand { LocationX = CanvasHeightMap, LocationY = CanvasHeightMap };
+            Track.Children.Add(sand);
+
+            //add first car
+
+            car1 = new Car1 { LocationX = CanvasWidth / 4, LocationY = CanvasHeight / 4 };
+            Track.Children.Add(car1);
+            car1.Updateposition();
+
+
+            // key listeners
+            Window.Current.CoreWindow.KeyDown += CoreWindow_KeyDown;
+            Window.Current.CoreWindow.KeyUp += CoreWindow_KeyUp;
+
+            // Game fps
+            game = new DispatcherTimer();
+            game.Tick += Game_Timer;
+            game.Interval = new TimeSpan(0, 0, 0, 0, 600 / 70);
+            game.Start();
+        }
+               private void Game_Timer(object sender, object e)
+        {
+            //move
+
+            if (Up) car1.Drive();
+            if (Down) car1.Drive1();
+            //spinning
+            if (Left) car1.Rotate(-3);
+            if (Right) car1.Rotate(3);
+
+
+            car1.Updateposition();
+        }
+
+        // see if the controls are not pressed
+        private void CoreWindow_KeyUp(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            switch (args.VirtualKey)
+            {
+                case VirtualKey.Up:
+                    Up = false;
+                    break;
+
+                case VirtualKey.Left:
+                    Left = false;
+                    break;
+
+                case VirtualKey.Right:
+                    Right = false;
+                    break;
+
+                case VirtualKey.Down:
+                    Down = false;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        // if controls are pressed
+        private void CoreWindow_KeyDown(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            switch (args.VirtualKey)
+            {
+                case VirtualKey.Up:
+                    Up = true;
+                    break;
+
+                case VirtualKey.Left:
+                    Left = true;
+                    break;
+
+                case VirtualKey.Right:
+                    Right = true;
+                    break;
+
+                case VirtualKey.Down:
+                    Down = true;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+              private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (rootFrame == null) return;
+
+            if (rootFrame.CanGoBack)
+            {
+                rootFrame.GoBack();
+            }
+        }
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (rootFrame == null) return;
+
+            if (rootFrame.CanGoBack)
+            {
+                rootFrame.GoBack();
+            }
+        }
+    }
+    }
+
